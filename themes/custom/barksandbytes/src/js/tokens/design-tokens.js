4f5e10a0183c22a0e20ee2cc4751e3f24d60bc1a
csnizik <csnizik@tulane.edu>
2023-04-20 15:57:41 -0500

    rm nm

---
+++ themes/custom/barksandbytes/src/js/tokens/design-tokens.js
@@ -0,0 +1,42 @@
+/**
+ * Do not edit directly
+ * Generated on Thu, 20 Apr 2023 20:51:51 GMT
+ */
+
+export const ColorPrimary = "#007bff";
+export const ColorSecondary = "#6c757d";
+export const ColorSuccess = "#28a745";
+export const ColorInfo = "#17a2b8";
+export const ColorWarning = "#ffc107";
+export const ColorDanger = "#dc3545";
+export const ColorLight = "#f8f9fa";
+export const ColorDark = "#343a40";
+export const LayoutSpacingSmall = "4px";
+export const LayoutSpacingMedium = "8px";
+export const LayoutSpacingLarge = "16px";
+export const LayoutSpacingXlarge = "32px";
+export const LayoutBorderRadiusDefault = "4px";
+export const FontFamilyPrimary = "euclid";
+export const FontWeightRegular = "400";
+export const FontWeightMedium = "500";
+export const FontWeightBold = "700";
+export const FontSize10 = "0.625rem";
+export const FontSize12 = "0.75rem";
+export const FontSize14 = "0.875rem";
+export const FontSize16 = "1rem";
+export const FontSize18 = "1.125rem";
+export const FontSize20 = "1.25rem";
+export const FontSize24 = "1.5rem";
+export const FontSize28 = "1.75rem";
+export const FontSize32 = "2rem";
+export const FontSize36 = "2.25rem";
+export const FontSize40 = "2.5rem";
+export const LineHeightBase = "1";
+export const LineHeightXs = "1.2";
+export const LineHeightSm = "1.22222";
+export const LineHeightMd = "1.25";
+export const LineHeightLg = "1.28571";
+export const LineHeightXl = "1.33333";
+export const LineHeightXxl = "1.4";
+export const LineHeightXxxl = "1.42857";
+export const LineHeightXxxxl = "1.5";
