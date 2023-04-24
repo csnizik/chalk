7169dec40795d63a7b3bb3083dc864d928646a5c
csnizik <csnizik@tulane.edu>
2023-04-24 15:41:42 -0500

    color test

--- themes/custom/chalk/utils/scripts/generateColorSuggestions.js
+++ themes/custom/chalk/utils/scripts/generateColorSuggestions.js
@@ -3,32 +3,38 @@ const processTokens = require('../processTokens')
 function generateColorSuggestions(obj) {
   for (const key in obj) {
     if (typeof obj[key] === 'object') {
-      processTokens.generateContrasts(obj[key])
+      generateColorSuggestions(obj[key])
     } else if (key === 'background') {
-      if (obj.trueHeading === '' || obj.trueText === '') {
-        try {
-          throw new Error(
-            'ERROR: You can not leave empty strings in any trueHeading or trueText field. Either enter a token name or remove/comment out the line.'
-          )
-        } catch (err) {
-          console.error(err.message)
-          return
-        }
-      }
+      const contrast = processTokens.generateContrasts(obj[key])
+      obj.contrast = contrast
+      // if (
+      //   (obj.trueHeading && obj.trueHeading === '') ||
+      //   (obj.truText && obj.trueText === '')
+      // ) {
+      //   console.log('fuckup')
+      //   try {
+      //     throw new Error(
+      //       'trueHeading or trueText is empty. Please provide a value.'
+      //     )
+      //   } catch (err) {
+      //     console.error(err.message)
+      //     return
+      //   }
+      // }
 
-      if (!obj.trueHeading) {
-        const contrastHeading = processTokens.generateContrasts(obj[key])
-        obj.heading = contrastHeading
-      } else {
-        obj.heading = obj.trueHeading
-      }
+      // if (!obj.trueHeading) {
+      //   const contrastHeading = processTokens.generateContrasts(obj[key])
+      //   obj.heading = contrastHeading
+      // } else {
+      //   obj.heading = obj.trueHeading
+      // }
 
-      if (!obj.trueText) {
-        const contrastText = processTokens.generateContrasts(obj[key])
-        obj.text = contrastText
-      } else {
-        obj.text = obj.trueText
-      }
+      // if (!obj.trueText) {
+      //   const contrastText = processTokens.generateContrasts(obj[key])
+      //   obj.text = contrastText
+      // } else {
+      //   obj.text = obj.trueText
+      // }
     }
   }
 }
