39cfdd50549955f120d79e2e80830870d031a4f0
csnizik <csnizik@tulane.edu>
2023-04-26 09:36:01 -0500

    added shadow levels

--- themes/custom/chalk/design-tokens/2-context/box-shadow.js
+++ themes/custom/chalk/design-tokens/2-context/box-shadow.js
@@ -1,9 +1,17 @@
 const processTokens = require('../../utils/processTokens')
 
+const boxShadowColor = 'rgba(46, 46, 69, '
+
 const boxShadows = {
-  elevated1: `0 $foun-spacing-xxxs $foun-spacing-xxxs $foun-color-neutral-gray-20`,
-  elevated2: `$foun-spacing-xxxs $foun-spacing-xxs $foun-spacing-xxs $foun-color-neutral-gray-20`,
-  // elevatedCurled,
+  elevated1: `$foun-spacing-xxxs $foun-spacing-xxxs $foun-spacing-xxs ${boxShadowColor}0.2)`,
+  elevated2: `$foun-spacing-xxs $foun-spacing-xxs $foun-spacing-xxs ${boxShadowColor}0.3)`,
+  elevated3: `$foun-spacing-xxs $foun-spacing-xxs $foun-spacing-xs ${boxShadowColor}0.4)`,
+  elevated4: `$foun-spacing-xs $foun-spacing-xs $foun-spacing-xs ${boxShadowColor}0.5)`,
+  elevated6: `$foun-spacing-xs $foun-spacing-sm $foun-spacing-sm ${boxShadowColor}0.6)`,
+  elevated8: `$foun-spacing-sm $foun-spacing-md $foun-spacing-md ${boxShadowColor}0.7)`,
+  elevated12: `$foun-spacing-md $foun-spacing-lg $foun-spacing-lg ${boxShadowColor}0.8)`,
+  elevated16: `$foun-spacing-lg $foun-spacing-xl $foun-spacing-xl ${boxShadowColor}0.9)`,
+  elevated24: `$foun-spacing-xl $foun-spacing-xxl $foun-spacing-xxl ${boxShadowColor}1)`,
 }
 
 module.exports = { contBoxShadows: processTokens.generateTokens(boxShadows) }
