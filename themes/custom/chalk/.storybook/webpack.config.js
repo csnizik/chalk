3d1e7f31e47f5b4d95a744fef5f1c9995c7c9ae8
csnizik <csnizik@tulane.edu>
2023-04-26 20:02:24 -0500

    adding sb

---
+++ themes/custom/chalk/.storybook/webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path')
+
+module.exports = ({ config }) => {
+  // Twig:
+  config.module.rules.push({
+    test: /\.twig$/,
+    use: [
+      {
+        loader: 'twig-loader',
+      },
+    ],
+  })
+  return config
+}
